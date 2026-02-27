f3700c01de52b5a9b017710107c6c2cc10ced57f
andreeag <andreea.ganeanu@student.tuiasi.ro>
2026-02-27 13:14:48 +0200

    commit2

--- prjocetLab2/src/prjocetLab2/Clasa1.java
+++ prjocetLab2/src/prjocetLab2/Clasa1.java
@@ -5,6 +5,7 @@ public class Clasa1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("buna prieteni");
+			System.out.println("ce faceti?");
 	}
 
 }
